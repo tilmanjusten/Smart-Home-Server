6255cca877e2b10401ac87e8c044569c650ee46d
Tilman Justen <git@tilmanjusten.de>
2018-09-18 16:56:48 +0200

    render chart data

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,16 @@
 const socket = io('http://localhost:3124');
 const history = {}
+const charts = {
+    'INKE': null,
+    'PURL': null,
+    'ODIN': null
+}
+const deviceNames = {
+    'INKE': 'Schlafzimmer',
+    'PURL': 'Badezimmer',
+    'ODIN': 'Wohnzimmer'
+}
+
 
 socket.emit('get status')
 socket.emit('get history')
@@ -14,6 +25,7 @@ socket.on('status', status => {
 
 socket.on('update', item => {
     addItemToHistory(item)
+    addChartItem(item)
     updateGui(item)
 })
 
@@ -26,10 +38,25 @@ function addItemToHistory(item) {
         history[item.deviceId] = []
     }
 
+    item.date = new Date(item.date)
+
     history[item.deviceId].push(item)
 
     // crop history for every device
     history[item.deviceId] = history[item.deviceId].slice(-240)
+}
+
+function addChartItem(item) {
+    charts[item.deviceId].push([
+        {
+            time: item.date.getTime() / 1000,
+            y: item.hu * 1
+        },
+        {
+            time: item.date.getTime() / 1000,
+            y: item.te * 1
+        }
+    ])
 }
 
 function updateGui(item) {
@@ -59,12 +86,45 @@ function showError(item) {
 socket.on('history', data => {
     data.forEach(addItemToHistory)
 
+
     // show latest data from history on page load
     for (const deviceId in history) {
         if (history.hasOwnProperty(deviceId) && typeof history[deviceId] === 'object') {
             const item = history[deviceId].pop()
 
             updateGui(item)
+        }
+    }
+
+    // chart data preparation
+    for (const deviceId in history) {
+        if (history.hasOwnProperty(deviceId) && typeof history[deviceId] === 'object') {
+            const device = history[deviceId]
+            const chartData = []
+
+            console.log(device.length)
+
+            chartData.push(
+                {
+                    label: `Luftfeuchtigkeit ${deviceNames[deviceId]}`,
+                    values: []
+                },
+                {
+                    label: `Temperatur ${deviceNames[deviceId]}`,
+                    values: []
+                }
+            )
+
+            charts[deviceId] = $(`#chart-${deviceId.toLowerCase()}`).epoch({
+                type: 'time.line',
+                data: chartData,
+                axes: ['left', 'right', 'bottom'],
+                windowSize: 120,
+                historySize: 240,
+                range: [0, 100]
+            });
+
+            device.forEach(addChartItem)
         }
     }
 })
